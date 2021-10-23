0ada67283a5a422fdd6eaf2284305a524ad17935
NAKAI Ryosuke <nakai@shiguredo.jp>
2021-10-23 12:38:40 +0900

    いろいろ

--- src/cowboy_ws_app.erl
+++ src/cowboy_ws_app.erl
@@ -1,8 +1,3 @@
-%%%-------------------------------------------------------------------
-%% @doc cowboy_ws public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(cowboy_ws_app).
 
 -behaviour(application).
@@ -10,9 +5,18 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+                                      {'_', [
+                                             {"/", cowboy_static, {priv_file, cowboy_ws, "index.html"}},
+                                             {"/echo", ws_echo_h, []},
+                                             {"/static/[...]", cowboy_static, {priv_dir, cowboy_ws, "static"}}
+                                            ]}
+                                     ]),
+    {ok, _} = cowboy:start_clear(http, [{port, 3000}], #{
+                                                         env => #{dispatch => Dispatch}
+                                                        }),
     cowboy_ws_sup:start_link().
+
 
 stop(_State) ->
     ok.
-
-%% internal functions
